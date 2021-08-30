692b23a60e83f602220cd40df8e36489e4481db7
mehmeteminaydin <meaydin.ceng@gmail.com>
2021-08-30 06:56:21 -0400

    editted file name

--- scrapy_project/wikipedia_scraping/settings.py
+++ scrapy_project/wikipedia_scraping/settings.py
@@ -12,7 +12,7 @@ BOT_NAME = 'tutorial'
 SPIDER_MODULES = ['tutorial.spiders']
 NEWSPIDER_MODULE = 'tutorial.spiders'
 ITEM_PIPELINES = {'scrapy.pipelines.images.ImagesPipeline': 1}
-IMAGES_STORE = 'local_folder2'
+IMAGES_STORE = 'downloaded_images'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
